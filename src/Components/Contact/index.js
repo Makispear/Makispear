405cf21fc7fdbb4f3cb5cf8406855de63f5e664a
Maki Aboabida <maki-miko@hotmail.com>
2021-12-08 18:41:25 -0500

    Refactored: logos are now dynamic

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,30 +1,44 @@
 import React from "react";
  
 function Contact() {
+  const socialMedia = [
+    {
+      name: 'GitHub',
+      src: 'github.png',
+      href: 'https://github.com/Makispear',
+    },
+    {
+      name: 'LinkedIn',
+      src: 'linkedin.png',
+      href: 'http://www.linkedin.com/in/mekki-abo-obaida-59a48b191'
+    },
+    {
+      name: 'Outlook',
+      src: 'outlook.svg',
+      href: 'mailto:maki-miko@hotmail.com?subject=I Visited Your Online Portfolio!!!&body=Hi Maki! I like your Portfolio!'
+    },
+    {
+      name: 'Whatsapp',
+      src: 'whatsapp.png',
+      href: 'https://wa.me/18607941672?text=Hey Maki! I like your online portfolio!'
+    }
+  ]
+
   return (
     <section id="contact">
         <h2 className="section-title">Contact</h2>
         <ul>
-            <li>
-                <a target="_blank" href="https://github.com/Makispear">
-                    <img  className="logos" src="assets/images/GitHub-Mark-Light-32px.png" alt="GitHub" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" href="http://www.linkedin.com/in/mekki-abo-obaida-59a48b191">
-                    <img  className="logos" id="linkedIn" src="assets/images/LI-In-Bug.png" alt="LinkedIn" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" href="mailto:maki-miko@hotmail.com?subject=I Visited Your Online Portfolio!!!&body=Hi Maki! I like your Portfolio!">
-                    <img  className="logos" id="outlook" src="assets/images/Outlook.com-Logo.wine.svg" alt="Outlook" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" href="https://wa.me/18607941672?text=Hey Maki! I like your online portfolio!">
-                    <img  className="logos" id="whatsapp" src="assets/images/whatsapp_logo_PNG3.png" alt="Whatsapp" />
-                </a>
-            </li>
+          {
+            socialMedia.map(item => {
+              return (
+                <li>
+                    <a target="_blank" href={item.href}>
+                        <img title={item.name} className="logos" id={item.name.toLowerCase()} src={require(`../../assets/images/${item.src}`).default } alt={item.name} />
+                    </a>
+                </li>
+              )
+            })
+          }
         </ul>
     </section>
   )
--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 function Hero() {
+
   return (
     <section id="hero">
-      <img src="assets/images/measdfvb.jpg" alt="Picture of Maki smiling with A river in the back" />
+      <img src={require(`../../assets/images/herome.jpg`).default } alt="Picture of Maki smiling with A river in the back" />
       <h1 data-testid="name">Maki Aboabida</h1>
       <h2 data-testid="occupation">Full-Stack Web Developer</h2>
     </section>
