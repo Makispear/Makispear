9086dc50a884cf71f36949155eec1b74e2f08370
Maki Aboabida Maki <maki-miko@hotmail.com>
2022-03-10 22:11:42 -0500

    Added code for form | not working yet

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import { validateEmail } from '../../utils/helpers'
- 
-function Contact() {
+import emailjs from 'emailjs-com';
+import { init } from '@emailjs/browser';
+require('dotenv').config();
+init("CtPuJjxAL86EIc0yB");
 
-  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-  const { name, email, message } = formState;
+function Contact() {
+  const [formState, setFormState] = useState({ name: '', subject: '', email: '', message: '' });
+  let { name, subject, email, message } = formState;
   const [errorMessage, setErrorMessage] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     console.log(formState);
+
+    (function () {
+      // https://dashboard.emailjs.com/admin/integration
+      emailjs.init("CtPuJjxAL86EIc0yB");
+    })();
+
+    emailjs.send(process.env.serviceID, process.env.templateID, formState, process.env.userID)
+      .then(result => {
+        console.log(result.text);
+      }, function (error) {
+        console.log('FAILED...', error.text);
+      });
+
+    e.target.reset()
   }
 
   const handleChange = (e) => {
@@ -26,7 +43,7 @@ function Contact() {
           setErrorMessage('');
         }
       }
-    } 
+    }
     if (!errorMessage) {
       setFormState({ ...formState, [e.target.name]: e.target.value });
     }
@@ -35,28 +52,32 @@ function Contact() {
 
   return (
     <section id="contact">
-        <h2 className="section-title">Contact</h2>
-        <form id='contactForm' className="contact-form" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name" defaultValue={name}>Name:</label>
-            <input name="name"onBlur={handleChange} />
-          </div>
-          <div>
+      <h2 className="section-title">Contact</h2>
+      <form id='contactForm' className="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name" defaultValue={name}>Name:</label>
+          <input name="name" onBlur={handleChange} required />
+        </div>
+        <div>
+          <label htmlFor="subject" defaultValue={subject}>Subject:</label>
+          <input name="subject" onBlur={handleChange} required />
+        </div>
+        <div>
           {errorMessage && (
             <div>
               <p className="error-text">{errorMessage}</p>
             </div>
           )}
-            <label htmlFor="email" defaultValue={email}>Email:</label>
-            <input name="email" onBlur={handleChange} />
-          </div>
-          <div>
-            <label htmlFor="message" defaultValue={message}>Message:</label>
-            <textarea name="message" onBlur={handleChange} />
-          </div>
-          
-          <button className='btn' type="submit">Submit</button>
-        </form>
+          <label htmlFor="email" defaultValue={email}>Email:</label>
+          <input name="email" onBlur={handleChange} required />
+        </div>
+        <div>
+          <label htmlFor="message" defaultValue={message}>Message:</label>
+          <textarea name="message" onBlur={handleChange} required />
+        </div>
+
+        <button className='btn' type="submit">Submit</button>
+      </form>
     </section>
   )
 }
